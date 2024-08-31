414d91b5e6e1ed3d85b363b9aaeacca7ad48535f
Candra Laksana Pratama Putra <candraputra21ch@gmail.com>
2024-08-31 13:54:55 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(
+    "#element",
+    {
+        strings: [
+            "Web Developer", 
+            "Designer", 
+            "Freelancer", 
+            "Content Creator", 
+        ],
+        typeSpeed: 100,
+        loop: true,
+        shuffle: true,
+    }
+)
